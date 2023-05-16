cf43e4b0c87f9bca0148a477e192ff99be720c32
Nese Yildirim <neseyild@student.arteveldehs.be>
2023-05-16 10:31:23 +0200

    merch conflict fix

--- src/models/Role.js
+++ src/models/Role.js
@@ -1,26 +1,43 @@
-import typeorm from "typeorm";
-
 const { EntitySchema } = typeorm;
+
 export default new EntitySchema({
-  name: 'role',
-  tableName: 'role',
-  columns: {
-    id_role: {
-      type: 'int',
-      primary: true,
-    },
-    label: {
-      type: "varchar",
-    },
-  },
-  relations: {
-    staf: {
-      target: "staf",
-      type: "one-to-many",
-      joinColumn: {
-        name: "role_id",
-      },
-      onDelete: "CASCADE",
-    },
-  },  
+
+  name: 'role',
+
+  tableName: 'role',
+
+  columns: {
+
+    id: {
+
+      type: 'int',
+
+      primary: true,
+
+    },
+
+    label: {
+
+      type: "varchar",
+
+    },
+
+  },
+
+  relations: {
+
+    staf: {
+
+      target: "staf",
+
+      type: "one-to-many",
+
+      cascade: true,
+
+      inverseSide: "role"
+
+    },
+
+  },  
+
 });
--- src/models/Staf.js
+++ src/models/Staf.js
@@ -1,37 +1,76 @@
 import typeorm from "typeorm";
 
+
+
+
 const { EntitySchema } = typeorm;
+
 export default new EntitySchema({
-  name: 'staf',
-  tableName: 'staf',
-  columns: {
-    id_staf: {
-      type: 'int',
-      primary: true,
-    },
-    email: {
-      type: 'varchar',
-    },
-    password: {
-      type: 'varchar',
-    },
-    avatar: {
-      type: 'varchar',
-      nullable: true,
-    },
-  },
-  relations: {
-    role: {
-      target: "role",
-      type: "many-to-one",
-      cascade: true,
-      inverseSide: "staf",
-    },
-    meta: {
-      target: "usermeta",
-      type: "one-to-one",
-      cascade: true,
-      inverseSide: "staf",
-    },
-  },
+
+  name: 'staf',
+
+  tableName: 'staf',
+
+  columns: {
+
+    id_staf: {
+
+      type: 'int',
+
+      primary: true,
+
+    },
+
+    email: {
+
+      type: 'varchar',
+
+    },
+
+    password: {
+
+      type: 'varchar',
+
+    },
+
+    avatar: {
+
+      type: 'varchar',
+
+      nullable: true,
+
+    },
+
+  },
+
+  relations: {
+
+    role: {
+
+      target: "role",
+
+      type: "many-to-one",
+
+      joinColumn: {
+
+        name: "role_id",
+
+      }
+
+    },
+
+    meta: {
+
+      target: "usermeta",
+
+      type: "one-to-one",
+
+      cascade: true,
+
+      inverseSide: "staf",
+
+    },
+
+  },
+
 });
