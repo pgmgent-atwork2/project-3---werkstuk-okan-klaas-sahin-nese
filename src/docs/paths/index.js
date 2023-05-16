c64b1cd92e09444c84f1d193e3e44a7c126b5f9a
Nese Yildirim <neseyild@student.arteveldehs.be>
2023-05-16 11:59:02 +0200

    add role on register

--- src/docs/paths/index.js
+++ src/docs/paths/index.js
@@ -1,1 +1,8 @@
-export default {}
+// import Students from './students.js';
+// import Staf from './staf.js';
+// import Exercises from './exercises.js';
+// import Commands from './commands.js'
+
+export default {
+
+}
